628c97f128062daf213c8ebb4150deb972f91396
Luciano Muniz <luciano.mo@gmail.com>
2022-05-24 18:27:35 -0300

    added minor ajusts in the API responses and input validations

--- server/src/routes/people/people.controller.js
+++ server/src/routes/people/people.controller.js
@@ -8,64 +8,84 @@ const {
 } = require('../../models/people.model');
 
 async function httpGetAllPeople(req, res) {
-  return res.status(200).json(await getAllPeople());
+  try {
+    return res.status(200).json(await getAllPeople());
+  } catch (e) {
+    return res.status(400).json({ ok: 'false' });
+  }
 }
 
 async function httpGetPerson(req, res) {
-  const person = await getPersonById(req.params.id);
-  return res.status(200).json(person);
+  try {
+    const personId = Number(req.params.id);
+    const person = await getPersonById(personId);
+    return res.status(200).json(person);
+  } catch (e) {
+    return res.status(400).json({ ok: 'false' });
+  }
 }
 
 async function httpAddNewPerson(req, res) {
-  const person = req.body;
+  try {
+    const person = req.body;
+    if (!person.name || !person.phones) {
+      return res.status(400).json({
+        error: 'Missing required person property',
+      });
+    }
 
-  if (!person.name || !person.phones) {
-    return res.status(400).json({
-      error: 'Missing required person property',
-    });
+    await addNewPerson(person);
+    return res.status(201).json({ ok: 'true' });
+  } catch (e) {
+    return res.status(400).json({ ok: 'false' });
   }
-
-  await addNewPerson(person);
-  return res.status(201).json(person);
 }
 
 async function httpUpdatePerson(req, res) {
-  const person = req.body;
-  const personId = Number(req.params.id);
+  try {
+    const person = req.body;
+    const personId = Number(req.params.id);
 
-  const existsPerson = await existsPersonWithId(personId);
-  if (!existsPerson || person.id !== personId) {
-    return res.status(404).json({
-      error: 'Person not found or property issue',
-    });
+    const existsPerson = await existsPersonWithId(personId);
+    if (!existsPerson || person.id !== personId) {
+      return res.status(404).json({
+        error: 'Person not found or property issue',
+      });
+    }
+
+    if (!person.name || !person.phones) {
+      return res.status(400).json({
+        error: 'Missing required person property',
+      });
+    }
+
+    await updatePerson(person);
+    return res.status(201).json({ ok: 'true' });
+  } catch (e) {
+    return res.status(400).json({ ok: 'false' });
   }
-
-  if (!person.name || !person.phones) {
-    return res.status(400).json({
-      error: 'Missing required person property',
-    });
-  }
-
-  await updatePerson(person);
-  return res.status(201).json(person);
 }
 
 async function httpDeletePerson(req, res) {
-  const personId = Number(req.params.id);
+  try {
+    const personId = Number(req.params.id);
 
-  const existsPerson = await existsPersonWithId(personId);
-  if (!existsPerson) {
-    return res.status(404).json({
-      error: 'Person not found',
-    });
+    const existsPerson = await existsPersonWithId(personId);
+    if (!existsPerson) {
+      return res.status(404).json({
+        error: 'Person not found',
+      });
+    }
+
+    const deleted = await deletePersonById(personId);
+    if (!deleted) {
+      return res.status(400).json({ error: 'Person not deleted' });
+    }
+
+    return res.status(200).json({ ok: 'true' });
+  } catch (e) {
+    return res.status(400).json({ ok: 'false' });
   }
-
-  const deleted = await deletePersonById(personId);
-  if (!deleted) {
-    return res.status(400).json({ error: 'Person not deleted' });
-  }
-
-  return res.status(200).json({ ok: 'true' });
 }
 
 module.exports = {
